2c5eba9f27294094a7f745e509a18c04b84e565e
ln-dev7 <leonelngoya@gmail.com>
2022-11-06 15:48:03 +0100

    redesign form delete

--- src/Components/FormConfirm.svelte
+++ src/Components/FormConfirm.svelte
@@ -13,12 +13,14 @@
 </script>
 
 <div on:click={ferModalSupp} class="overlay" />
-<form class="carte mt-5" on:submit|preventDefault={confSupp} on:reset|preventDefault={ferModalSupp}>
-  <div class="form-group mb-3">
+<form class="form" on:submit|preventDefault={confSupp} on:reset|preventDefault={ferModalSupp}>
+  <div>
       <p>Voulez vous vraiment supprimer ?</p>
   </div>
-  <button type="submit" class="btn btn-danger">Oui</button>
-  <button type="reset" class="btn btn-success">Non</button>
+  <div>
+    <button type="submit" class="yes">Oui</button>
+  <button type="reset" class="no">Non</button>
+  </div>
 </form>
 
 <style>
@@ -28,16 +30,46 @@
     background: rgba(0, 0, 0, 0.5);
     z-index: 5;
   }
-  .carte {
+  .form {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
     max-width: 600px;
-    width: 97%;
-    border: 1px solid #333;
-    padding: 15px;
+    width: 95%;
+    border-radius: 0.5rem;
+    padding: 2rem;
     background: #fff;
     position: fixed;
     top: 40%;
     left: 50%;
     transform: translate(-50%, -50%);
     z-index: 5;
+  }
+
+  p {
+    width: 100%;
+    text-align: center;
+    font-weight: 600;
+    font-size: 1.2rem;
+  }
+
+  button {
+    width: 100%;
+    font-weight: 500;
+    margin-top: 0.5rem;
+    padding: 0.5rem 1rem;
+    border: none;
+    border-radius: .25rem;
+    cursor: pointer;
+  }
+
+  .yes {
+    background: #f0214ad0;
+    color: #fff;
+  }
+
+  .no {
+    background: #4f46e5;
+    color: #fff;
   }
 </style>
